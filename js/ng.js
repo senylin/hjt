9a3dbeabfb56d2b02e588754c78a10b6c2e79365
senylin <939525717@qq.com>
2016-04-02 13:56:28 +0800

    'login2'

--- js/ng.js
+++ js/ng.js
@@ -10,6 +10,27 @@ document.onscroll =function(){
     }
 
 }
+function loadLogin(page){
+    var mexbox=$.ajax({url:page,async:false});
+    $("#login").html(mexbox.responseText);
+
+//    var xmlhttp;
+//    if (window.XMLHttpRequest){
+//        xmlhttp=new XMLHttpRequest();
+//    }else{
+//        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+//    }
+//    xmlhttp.onreadystatechange=function(){
+//        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+//        {
+//            document.getElementById("login").innerHTML=xmlhttp.responseText;
+//        }
+//    }
+//
+//    xmlhttp.open("GET",page,true);
+//    xmlhttp.send();
+//    $('#drag').drag();
+}
 var app = angular.module('lsyApp',['ngAnimate']);
 app.controller('lmodel1',function($scope){
     $scope.tbcount = 1;
