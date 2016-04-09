eb83a88bc16048dd14b2693a4cd41d49c82ca80f
senylin <939525717@qq.com>
2016-04-09 14:22:23 +0800

    'fenli'

--- js/ng.js
+++ js/ng.js
@@ -10,6 +10,16 @@ document.onscroll =function(){
     }
 
 }
+//window.onload=function(){
+//    var mainbox=$.ajax({url:"hxs.html",async:false});
+//    $("#mainbx").html(mainbox.responseText);
+////    {
+////        if(statusTxt=="success")
+//////            alert("外部内容加载成功!");
+////        if(statusTxt=="error")
+////            alert("Error: "+xhr.status+": "+xhr.statusText);
+////    });
+//}
 function loadLogin(page){
     var mexbox=$.ajax({url:page,async:false});
     $("#login").html(mexbox.responseText);
@@ -32,6 +42,40 @@ function loadLogin(page){
 //    $('#drag').drag();
 }
 var app = angular.module('lsyApp',['ngAnimate']);
+app.controller('lbody',function($scope){
+    $scope.mbody='hgw.html';
+    $scope.noc=1;
+    $scope.count = $scope.noc;
+    $scope.msglist=true;
+    $scope.msgtoggle=function(){
+        $scope.msglist=!$scope.msglist;
+    }
+    $scope.choose=function(count,value){
+        if(count==value)
+        {
+            return true;
+        }else return false;
+    }
+    $scope.goto=function(count){
+        if(count==0)return;
+        var body= document.body;
+        var top = 550+(count-1)*430;
+        body.scrollTop=550+(count-1)*430;
+    }
+    $scope.goto2=function(count){
+        if(count==0)return;
+        //  alert( document.body.scrollTop);
+        var body= document.body;
+
+        var top = 550+(count-1)*530;
+        body.scrollTop=550+(count-1)*530;
+    }
+    $scope.totop=function(){
+        var body= document.body;
+        body.scrollTop=150;
+    }
+
+});
 app.controller('lmodel1',function($scope){
     $scope.tbcount = 1;
     $scope.jmcount = 1;
@@ -102,8 +146,9 @@ app.controller('lpic',function($scope, $interval){
         $scope.number ;
     }, 5000);
 });
-app.controller('lnav1',function($scope){
-    $scope.count =1;
+app.controller('lnav',function($scope){
+    $scope.noc=1;
+    $scope.count = $scope.noc;
     $scope.msglist=true;
     $scope.msgtoggle=function(){
         $scope.msglist=!$scope.msglist;
@@ -162,11 +207,7 @@ app.controller('lnav3',function($scope){
         if(count==0)return;
         //  alert( document.body.scrollTop);
         var body= document.body;
-        //   var top=  angular.element(body).animate({ scrollTop: 550 }, 100);
-//            $animate.enter(document.body).then(function() {
-//                $scope.scrollTop = 550;
-//            });
-        // alert(top);
+
         var top = 550+(count-1)*530;
         body.scrollTop=550+(count-1)*530;
     }
